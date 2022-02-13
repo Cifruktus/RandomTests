76e186c8668611174c81a21e164fdeba257b96a3
Cifruktus <9028029+Cifruktus@users.noreply.github.com>
2022-02-13 23:02:24 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,9 +26,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3e803c3ecfbec5ae20ee4b61c6d80719",
-"/": "3e803c3ecfbec5ae20ee4b61c6d80719",
-"main.dart.js": "268b962fc93bee4907ec26fad12968c6",
+"index.html": "6ef360da2250a4f5155bfd5d6a209bca",
+"/": "6ef360da2250a4f5155bfd5d6a209bca",
+"main.dart.js": "d77f2bc9d03e46deb0813fa892913462",
 "manifest.json": "0567201f1f510a6af2f372f309dfe098",
 "version.json": "65dc3077ec85b9e59736032ae653d155"
 };
